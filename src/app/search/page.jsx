3b215e58dfd34c6c6184cf9377d3fd3c96b376fb
Wirya40 <wirya.atmaja2008@gmail.com>
2025-11-25 18:34:59 +0700

    first commit

--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -1,4 +1,7 @@
 "use client";
+
+export const dynamic = "force-dynamic";
+
 import { useSearchParams } from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
@@ -8,18 +11,17 @@ const products = [
   { id: 2, name: "Mesin Cuci - LG 7 Kg Front Load", price: 4100000, image: "/images/mesin cuci.jpg" },
   { id: 3, name: "Dispenser - Polytron Bottom Load", price: 1569000, image: "/images/dispenser.jpg" },
   { id: 4, name: "AC Split - Panasonic 1 PK", price: 4000000, image: "/images/ac.jpg" },
-  { id: 5, name: "Setrika - Philips GC160", price:  450000, image: "/images/setrika.jpg" },
+  { id: 5, name: "Setrika - Philips GC160", price: 450000, image: "/images/setrika.jpg" },
   { id: 6, name: "Rice Cooker - Philips 2L", price: 830000, image: "/images/rice.jpg" },
-  { id: 7, name: "Microwave - Sharp R-21D0(S)", price:  1050000, image: "/images/microwave.jpg" },
+  { id: 7, name: "Microwave - Sharp R-21D0(S)", price: 1050000, image: "/images/microwave.jpg" },
   { id: 8, name: "Blender - Miyako BL-101PL", price: 230000, image: "/images/blender.jpg" },
-  { id: 9, name: "Kompor Gas - Rinnai 2 Tungku", price:  560000, image: "/images/kompor.jpg" },
+  { id: 9, name: "Kompor Gas - Rinnai 2 Tungku", price: 560000, image: "/images/kompor.jpg" },
   { id: 10, name: "TV LED - Samsung 32 Inch", price: 2488000, image: "/images/tv.jpg" },
 ];
 
-
 export default function SearchPage() {
-  const params = useSearchParams();
-  const query = params.get("query") || "";
+  const searchParams = useSearchParams();
+  const query = searchParams.get("query") || "";
 
   const results = products.filter((p) =>
     p.name.toLowerCase().includes(query.toLowerCase())
@@ -27,11 +29,9 @@ export default function SearchPage() {
 
   return (
     <div className="max-w-6xl mx-auto px-6 py-10">
-
-      {/* Tombol kembali */}
       <Link
         href="/"
-        className="inline-block mb-6 px-4 py-2 bg-blue-500 text-white rounded-lg shadow hover:bg-blue-600 transition"
+        className="inline-block mb-6 px-4 py-2 bg-blue-500 text-white rounded-lg shadow"
       >
         ← Kembali ke Home
       </Link>
@@ -49,7 +49,7 @@ export default function SearchPage() {
           <Link
             key={item.id}
             href={`/product/${item.id}`}
-            className="bg-white p-3 rounded-lg shadow hover:shadow-lg transition"
+            className="bg-white p-3 rounded-lg shadow hover:shadow-lg"
           >
             <div className="relative w-full h-36">
               <Image src={item.image} alt={item.name} fill className="object-contain rounded" />
